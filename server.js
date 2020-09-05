f68726cae6b3dab111a7d3cefdb4479c46aba1be
rickjp <3741326+RickJP@users.noreply.github.com>
2020-09-05 21:46:41 +0900

    MODIFY - Moved db connection to seperate dir

--- server.js
+++ server.js
@@ -1,39 +1,48 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
+import Messages from './dbMessages.js';
+import './db/index.js';
+
+import Pusher from 'pusher';
 
 // app config
 const app = express();
 const port = process.env.PORT || 9000;
 
+const pusher = new Pusher({
+  appId: '1067240',
+  key: 'a26cdf4116a20abcb492',
+  secret: '961f4dce3f789d6dd747',
+  cluster: 'ap3',
+  encrypted: true,
+});
+
 // middlewares
-
-// DB Config
-const host = process.env.DB_HOST;
-const user = process.env.DB_USER;
-const pw = process.env.DB_PASSWORD;
-const db_name = process.env.DB_NAME;
-const options = process.env.DB_OPTIONS;
-const con_url = `mongodb+srv://${user}:${pw}@${host}${db_name}${options}`;
-
-mongoose.connect(
-  con_url,
-  {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  },
-  () => {
-    console.log(`Connected to DB successfully`);
-  }
-);
-
-// ????
+app.use(express.json());
 
 // API routes
 app.get('/', (req, res) => {
-  res.status(200).send('hello there!');
+  res.status(200).send('I am alive!');
+});
+
+app.get('/api/v1/messages/sync', (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    }
+    res.status(200).send(data);
+  });
+});
+
+app.post('/api/v1/messages/new', (req, res) => {
+  const dbMessage = req.body;
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(`New messages created: \n ${data}`);
+    }
+  });
 });
 
 // listener
